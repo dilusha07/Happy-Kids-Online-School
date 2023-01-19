31d63088947a0a5abb0aef4b4902e0744b0e719e
dilusha07 <dilushasandaruwani07@gmail.com>
2023-01-18 22:24:09 -0800

    Implement welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomepageComponent } from './components/welcomepage/welcomepage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component:WelcomepageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
