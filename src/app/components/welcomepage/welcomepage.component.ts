2f575d3c52efd450882e6cd6b29fb074a3ad5392
dilusha07 <dilushasandaruwani07@gmail.com>
2023-01-19 16:38:19 -0800

    Implement welcome page

---
+++ src/app/components/welcomepage/welcomepage.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-welcomepage',
+  templateUrl: './welcomepage.component.html',
+  styleUrls: ['./welcomepage.component.scss']
+})
+export class WelcomepageComponent implements OnInit {
+
+  constructor(private _router : Router) { }
+
+  ngOnInit(): void {
+  }
+
+  navigate()
+  {
+    this._router.navigate(['/login']);
+  }
+
+}
