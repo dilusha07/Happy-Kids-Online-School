d73650086025650997083210999b4fa770bd2aa0
dilusha07 <dilushasandaruwani07@gmail.com>
2023-01-28 19:56:48 +0530

    Implement addchapter component

---
+++ src/app/components/addchapter/addchapter.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddchapterComponent } from './addchapter.component';
+
+describe('AddchapterComponent', () => {
+  let component: AddchapterComponent;
+  let fixture: ComponentFixture<AddchapterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddchapterComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddchapterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
