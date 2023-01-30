3d00395ac32cc872e65d31b33c0b84568f10d95f
peshaladushyanthika <pdushyanthika@gmail.com>
2023-01-30 21:00:24 +0530

    create coolfact component

---
+++ src/app/components/coolfacts/coolfacts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coolfacts',
+  templateUrl: './coolfacts.component.html',
+  styleUrls: ['./coolfacts.component.scss']
+})
+export class CoolfactsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
