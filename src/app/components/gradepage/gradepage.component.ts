6b234c26976297437230cbe4d34abdb693f16272
peshaladushyanthika <pdushyanthika@gmail.com>
2023-01-30 20:58:20 +0530

    Create grade component

---
+++ src/app/components/gradepage/gradepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gradepage',
+  templateUrl: './gradepage.component.html',
+  styleUrls: ['./gradepage.component.scss']
+})
+export class GradepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
