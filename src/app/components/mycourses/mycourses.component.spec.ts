67e4e67468273911573e06d0bd0fc594ac46ea65
dilusha07 <dilushasandaruwani07@gmail.com>
2023-02-03 08:14:58 +0530

    Implement my courses component

---
+++ src/app/components/mycourses/mycourses.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MycoursesComponent } from './mycourses.component';
+
+describe('MycoursesComponent', () => {
+  let component: MycoursesComponent;
+  let fixture: ComponentFixture<MycoursesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MycoursesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MycoursesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
